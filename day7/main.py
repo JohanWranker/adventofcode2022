6c59d4b675c6c891f17050e5a44672c5f183be32
Johan Wranker <johan@wranker.org>
2022-12-08 21:33:27 +0100

    day7

--- day7/main.py
+++ day7/main.py
@@ -1,9 +1,20 @@
 data = [l.rstrip() for l in open("day7/in.txt")]
 import re
 
-cwd_split = [""]
+cwd_split = ["/"]
 cwd = "/"
 sizes = {"/": 0}
+
+
+def store_filesize(cmd_split, file_size):
+
+    path = "/".join(cmd_split)
+    print(path)
+    sizes[path] += file_size
+    a = cmd_split[0:-1]
+    if len(a) > 0:
+        store_filesize(a, file_size)
+
 
 for line in data:
     if line[0] == "$":
@@ -13,9 +24,9 @@ for line in data:
         elif "$ cd .." in line:
             sub_size = sizes[cwd]
             cwd_split.pop()
-            cwd = "/".join(cwd_split)
-            if cwd == "//":
-                cwd = "/"
+            # cwd = "/".join(cwd_split)
+            # if cwd == "//":
+            #    cwd = "/"
         elif "$ cd " in line:
             dd = line.split(" ")
             cwd_split.append(dd[-1])
@@ -29,15 +40,27 @@ for line in data:
             (_, dir) = line.split(" ")
         else:
             (size, _) = line.split(" ")
-            sizes[cwd] = sizes[cwd] + int(size)
+            store_filesize(cwd_split, int(size))
+#            sizes[cwd] = sizes[cwd] + int(size)
+#
+#            sub_size = sizes[cwd]
+#            cwd_split.pop()
+#            cwd = "/".join(cwd_split)
+#            if cwd == "//":
+#                cwd = "/"
 
 
 print(sizes)
 sum = 0
+unused = 70000000 - sizes["/"]
+needed = 30000000 - unused
+close_match = 70000000
 for i in sizes:
-    if sizes[i] <= 100000:
-        sum += sizes[i]
-    else:
-        print(i, sizes[i])
+    if sizes[i] >= needed and sizes[i] < close_match:
+        close_match = sizes[i]
 
 print(sum)
+
+needed = 70000000 - 30000000
+print(needed)
+print(close_match)
