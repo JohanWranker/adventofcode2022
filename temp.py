c0c925f3853e0d58c0576dbcfab9fcde70e2d798
Johan Wranker <Johan@wranker.org>
2022-12-11 19:29:41 +0100

    dd

---
+++ temp.py
@@ -0,0 +1,15 @@
+import math
+
+val = [79, 98]
+s = [math.floor(i*19/23) for i in val ]
+
+val = [54, 65, 75, 74]
+s = [math.floor(i +6 /23) for i in val ]
+
+
+== After round 1 ==
+Monkey 0 inspected items 2 times.
+Monkey 1 inspected items 4 times.
+Monkey 2 inspected items 3 times.
+Monkey 3 inspected items 6 times.
+
