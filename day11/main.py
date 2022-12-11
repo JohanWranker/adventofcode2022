e2b8b6a88a432b58b3efe9342b8d7971dc491a86
Johan Wranker <Johan@wranker.org>
2022-12-11 19:29:36 +0100

    dddd

--- day11/main.py
+++ day11/main.py
@@ -1,8 +1,34 @@
 fd = open("day11/in.txt")
+fd2 = open("day11/table.txt")
 import math
 import re
 
+
+table = {}
+loop = 0
+while (True):
+    line = fd2.readline()
+    m = re.match(r"== .*\s(\d+) ==",line)
+    assert m
+    loop = int(m.group(1))
+    table[loop] = {}
+    for _ in range(4):
+        line = fd2.readline()
+        m = re.match(r"Monkey (\d) inspected items (\d+) times.",line)
+        assert m
+        table[loop][int(m.group(1))] = int(m.group(2))
+    
+    if loop == 10000:
+        break
+    fd2.readline()
+
+
+
 #items = {}
+div_by= 3
+
+def rules(val):
+ return math.floor(val/div_by)
 
 def multold(current,par):
     return current*current
@@ -83,7 +109,7 @@ class Monkey:
             del(self.my_items[0])
             item["op"] += 1
             item["level"] = (self.operation)(item["level"],self.operation_value)
-            item["level"] = math.floor(item["level"]/3)
+            item["level"] = rules("level")
 
             if  item["level"] % int(self.test_operation) == 0:
                 monkeys[self.throw_true].catch(item)
@@ -98,15 +124,25 @@ while (fd.readable()):
     monkeys.append(m)
     fd.readline()
 
-for i in range(20):
+for i in range(10):
     for monkey in monkeys:
         monkey.do()
+    if i in table:
+        is_ok = True
+        for m in table[i]:
+            oo = table[i][m]
+            if oo != monkeys[m].handled:
+                print(f"Monkey {m} exp{oo} got {monkeys[m].handled}")
+                is_ok = False
+        if not is_ok:
+            break
 
 [print(f"Monkey {m.name}: {m.p()}") for m in monkeys]
 a= sorted([m.handled for m in monkeys])
 print(a)
 print(a[-1]*a[-2])
 pass
+
 
 
 
